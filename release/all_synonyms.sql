3f3529593ee8723ce4147aef19f22ca0893c17d6
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-07-31 10:58:05 -0400

    cleanup

--- release/all_synonyms.sql
+++
@@ -1,4 +0,0 @@
--- GENERATED from build/build.sh DO NOT modify this file directly as all changes will be overwritten upon next build
--- Automated listing for database/synonyms
-prompt @../database/synonyms/ast_flow_issues.sql
-@../database/synonyms/ast_flow_issues.sql
