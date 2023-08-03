652d177355c5db4d9491c661597cbbf8103feeff
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-08-03 10:20:56 -0400

    tests folder

--- .local/scripts/export_tests_script.sql
+++ .local/scripts/export_tests_script.sql
@@ -13,7 +13,7 @@ prompt set trimspool off
 prompt set feedback off
 prompt set echo off
 
-select distinct apex_string.format(q'[spool standard_tests/%1/%0.json
+select distinct apex_string.format(q'[spool standard_tests/%1/tests/%0.json
 select svt_deployment.json_content_clob (p_table_name => 'EBA_STDS_STANDARD_TESTS', p_test_code => '%0') thejsonclob from dual;]', test_code, eba_stds.file_name(standard_category_name) ) stmt
 from v_eba_stds_standard_tests_export tet
 where published_yn = 'Y'
