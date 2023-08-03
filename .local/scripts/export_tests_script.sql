2bb9f02a7334fdbc63f39efdfb4bb9606ca89c65
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-08-03 10:34:24 -0400

    export standard json

--- .local/scripts/export_tests_script.sql
+++ .local/scripts/export_tests_script.sql
@@ -17,6 +17,11 @@ select distinct apex_string.format(q'[spool standard_tests/%1/tests/%0.json
 select svt_deployment.json_content_clob (p_table_name => 'EBA_STDS_STANDARD_TESTS', p_test_code => '%0') thejsonclob from dual;]', test_code, eba_stds.file_name(standard_name) ) stmt
 from v_eba_stds_standard_tests_export tet
 where published_yn = 'Y'
+union all
+select distinct apex_string.format(q'[spool standard_tests/%1/STANDARD-%1.json
+select svt_deployment.json_content_clob (p_table_name => 'V_EBA_STDS_STANDARD_TESTS_EXPORT', p_standard_id => '%0') thejsonclob from dual;]', standard_id, eba_stds.file_name(standard_name) ) stmt
+from v_eba_stds_standards tet
+where file_blob is not null
 order by 1;
 
 prompt spool off
