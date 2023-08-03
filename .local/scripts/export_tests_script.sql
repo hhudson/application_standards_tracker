597b268ca8c856789feb6bff6919ebbde4226558
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-08-03 12:38:43 -0400

    correcting sqlcl

--- .local/scripts/export_tests_script.sql
+++ .local/scripts/export_tests_script.sql
@@ -2,16 +2,12 @@ set long 100000
 set trimspool off
 set feedback off
 set echo off
+set pages 0
+set linesize 10
 
 define PROJECT_DIR = "standard_tests"
 
 spool &PROJECT_DIR/temp_script.sql
-
-prompt set pages 0
-prompt set linesize 10
-prompt set trimspool off
-prompt set feedback off
-prompt set echo off
 
 select apex_string.format(q'[spool standard_tests/%1/tests/%0.json
 select svt_deployment.json_content_clob (p_table_name => 'EBA_STDS_STANDARD_TESTS', p_test_code => '%0') thejsonclob from dual;]', test_code, eba_stds.file_name(standard_name) ) stmt
