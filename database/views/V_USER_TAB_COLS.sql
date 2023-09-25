313dd0627e163192e927711088b161621d282c57
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-09-25 18:20:51 -0400

    v_user_tab_cols

--- database/views/V_USER_TAB_COLS.sql
+++ database/views/V_USER_TAB_COLS.sql
@@ -7,7 +7,10 @@
 create or replace force editionable view V_USER_TAB_COLS as
 select owner,
        table_name, 
-       column_name 
+       column_name,
+       data_type,
+       char_used,
+       data_length
 from all_tab_cols
 where owner = case when sys_context('userenv', 'current_user') = svt_preferences.get_preference ('SVT_DEFAULT_SCHEMA')
                    then svt_ctx_util.get_default_user
