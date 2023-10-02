b26a2c0b7b68692b40139501fc072e15b20e40cc
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-10-02 11:17:51 -0400

    removing unused procedures

--- database/package_specs/EBA_STDS_FW.sql
+++ database/package_specs/EBA_STDS_FW.sql
@@ -1,32 +1,12 @@
 --liquibase formatted sql
 --changeset package_script:EBA_STDS_FW stripComments:false endDelimiter:/ runOnChange:true
 create or replace package eba_stds_fw authid definer as
-    function conv_txt_html (
-        p_txt_message in varchar2 )
-        return varchar2;
-    function conv_urls_links (
-        p_string in varchar2 )
-        return varchar2;
-    function format_status_update (
-        p_string in clob,
-        p_shorten_url in varchar2 default 'N' )
-        return varchar2;
-    function tags_cleaner (
-        p_tags  in varchar2,
-        p_case  in varchar2 default 'U' )
-        return varchar2;
     function get_preference_value (
         p_preference_name in varchar2 )
         return varchar2;
     procedure set_preference_value (
         p_preference_name  in varchar2, 
         p_preference_value in varchar2 );
-    function compress_int (
-        n in integer )
-        return varchar2;
-    function apex_error_handling (
-        p_error in apex_error.t_error )
-        return apex_error.t_error_result;
 end eba_stds_fw;
 /
 
