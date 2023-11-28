95776e9837aef93ab5bf73259b60dead7b359079
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-11-27 20:50:46 -0500

    dba_ to all_

--- database/views/public/V_USER_PLSQL_OBJECT_SETTINGS.sql
+++ database/views/public/V_USER_PLSQL_OBJECT_SETTINGS.sql
@@ -6,7 +6,7 @@
 
 create or replace force editionable view v_user_plsql_object_settings as
 select *
-from dba_plsql_object_settings
+from all_plsql_object_settings
 where owner = svt_ctx_util.get_default_user
 /
 --rollback drop view V_USER_PLSQL_OBJECT_SETTINGS;
