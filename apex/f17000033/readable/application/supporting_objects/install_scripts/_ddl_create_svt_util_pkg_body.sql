63db13d226efa7c29627f29aec766637dd34b7df
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-11-30 15:22:07 -0500

    SVT_ONE_REPORT

---
+++ apex/f17000033/readable/application/supporting_objects/install_scripts/_ddl_create_svt_util_pkg_body.sql
@@ -0,0 +1,133 @@
+  CREATE OR REPLACE EDITIONABLE PACKAGE BODY "SVT_UTIL" as
+----------------------------------------------------------------------------
+-- Copyright (c) Oracle Corporation 2020. All Rights Reserved.
+-- 
+-- NAME
+--   SVT_UTIL
+--
+-- DESCRIPTION
+--
+-- RUNTIME DEPLOYMENT: Yes
+--
+-- MODIFIED  (YYYY-MON-DD)
+-- hayhudso  2023-Nov-21 - created
+---------------------------------------------------------------------------- 
+  gc_scope_prefix constant varchar2(41) := lower($$plsql_unit) || '.';
+  gc_n constant varchar2(1) := 'N';
+  gc_y constant varchar2(1) := 'Y';
+  function app_name return varchar2
+  as
+  c_scope constant varchar2(128) := gc_scope_prefix || 'app_name';
+  c_debug_template constant varchar2(4000) := c_scope||' %0 %1 %2 %3 %4 %5 %6 %7';
+  c_db_name constant varchar2(100) := svt_preferences.get('SVT_DB_NAME');
+  c_app_name constant varchar2(100) := 'Standard Violation Tracker';
+  begin
+   
+   return case when c_db_name is null 
+               then c_app_name
+               else apex_string.format('[%s] %s', c_db_name, c_app_name)
+               end;
+  exception
+   when others then
+      apex_debug.error(p_message => c_debug_template, p0 =>'Unhandled Exception', p1 => sqlerrm, p5 => sqlcode, p6 => dbms_utility.format_error_stack, p7 => dbms_utility.format_error_backtrace, p_max_length=> 4096);
+     raise;
+  end app_name;
+  function alerts_yn return varchar2
+  as
+  c_scope constant varchar2(128) := gc_scope_prefix || 'alerts_yn';
+  c_debug_template constant varchar2(4000) := c_scope||' %0 %1 %2 %3 %4 %5 %6 %7';
+  l_alerts_yn varchar2(1) := gc_n;
+  l_alert_source varchar2(50) := 'NA';
+  begin
+   apex_debug.message(c_debug_template,'START');
+   
+  begin <<missing_data>>
+    if l_alerts_yn = gc_n then
+      select case when count(*) = 1
+                  then gc_y
+                  else gc_n
+                  end into l_alerts_yn
+          from sys.dual where exists (
+              select 1 
+              from v_svt_missing_base_data
+          );
+      apex_debug.info(c_debug_template, 'missing_data', l_alerts_yn);
+      if l_alerts_yn = gc_y then
+        l_alert_source := 'v_svt_missing_base_data';
+      end if;
+    end if;
+  end missing_data;
+  begin <<disabled_jobs>>
+    if l_alerts_yn = gc_n then
+      select case when count(*) = 1
+                  then gc_y
+                  else gc_n
+                  end into l_alerts_yn
+          from sys.dual where exists (
+              select 1
+              from apex_appl_automations
+              where polling_status_code = 'DISABLED'
+              and application_name = 'Standard Violation Tracker'
+          );
+      apex_debug.info(c_debug_template, 'disabled_jobs', l_alerts_yn);
+      if l_alerts_yn = gc_y then
+        l_alert_source := 'disabled_jobs';
+      end if;
+    end if;
+  end disabled_jobs;
+  begin <<failed_jobs>>
+    if l_alerts_yn = gc_n then
+      select case when count(*) = 1
+                  then gc_y
+                  else gc_n
+                  end into l_alerts_yn
+          from sys.dual where exists (
+              select 1 
+              from v_svt_automations_problems
+          );
+      apex_debug.info(c_debug_template, 'failed_jobs', l_alerts_yn);
+      if l_alerts_yn = gc_y then
+        l_alert_source := 'v_svt_automations_problems';
+      end if;
+    end if;
+  end failed_jobs;
+  begin <<missing_preferences>>
+    if l_alerts_yn = gc_n then
+      select case when count(*) = 1
+                  then gc_y
+                  else gc_n
+                  end into l_alerts_yn
+          from sys.dual where exists (
+              select 1 
+              from v_svt_preference_problems
+          );
+      apex_debug.info(c_debug_template, 'missing_preferences', l_alerts_yn);
+      if l_alerts_yn = gc_y then
+        l_alert_source := 'v_svt_preference_problems';
+      end if;
+    end if;
+  end missing_preferences;
+  begin <<problem_assignments>>
+    if l_alerts_yn = gc_n then
+      select case when count(*) = 1
+                  then gc_y
+                  else gc_n
+                  end into l_alerts_yn
+          from sys.dual where exists (
+              select 1 
+              from v_svt_problem_assignees
+          );
+      apex_debug.info(c_debug_template, 'problem_assignments', l_alerts_yn);
+      if l_alerts_yn = gc_y then
+        l_alert_source := 'v_svt_problem_assignees';
+      end if;
+    end if;
+  end problem_assignments;
+  return l_alerts_yn;
+  exception
+   when others then
+      apex_debug.error(p_message => c_debug_template, p0 =>'Unhandled Exception', p1 => sqlerrm, p5 => sqlcode, p6 => dbms_utility.format_error_stack, p7 => dbms_utility.format_error_backtrace, p_max_length=> 4096);
+     raise;
+  end alerts_yn;
+end SVT_UTIL;
+/ 
